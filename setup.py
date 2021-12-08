f6a4c973851e3ef85b95c6b780ec6304c8398323
chungyi.li <chungyi.li@ailabs.tw>
2021-12-08 16:48:44 +0800

    update some test configuration

--- setup.py
+++ setup.py
@@ -7,6 +7,26 @@ setup(
     author='Chung-Yi Li',
     author_email='leon129506@gmail.com',
     url='https://github.com/Chung-I/tsm',
-    packages=find_packages(),
-    install_requires=[]
+    packages=find_packages(
+        exclude=[
+            "*.tests",
+            "*.tests.*",
+            "tests.*",
+            "tests",
+            "test_fixtures",
+            "test_fixtures.*"
+        ]
+    ),
+    install_requires=[
+        'cn2an',
+        'regex',
+        'zhon',
+        'pandas',
+        'numpy',
+        'scipy',
+        'ckiptagger',
+        'opencc',
+        'tai5-uan5_gian5-gi2_kang1-ku7',
+    ],
+    include_package_data=True,
 )
