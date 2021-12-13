63d2e15e2d13c2187b1ec15e3e187ebf4251b1cc
chungyi.li <chungyi.li@ailabs.tw>
2021-12-13 15:39:23 +0800

    add some test cases, update g2p test

--- tests/g2p_test.py
+++ tests/g2p_test.py
@@ -3,13 +3,16 @@ import unittest
 from typing import List, Dict, Any
 import logging
 
+import re
 import nltk
 from nltk.tree import ParentedTree, Tree
 import json
 
+from tsm.chinese_head_finder import ChineseSemanticHeadFinder as ChineseHeadFinder
 from tsm.sentence import Sentence
-from tsm.g2p import infer_sandhi_boundary, lexical_government, get_src_sandhi_start_and_ends, infer_pron, alignment_to_tgt2src
-from tsm.g2p import cut_source_tokens_from_target_tokens_and_obtain_sandhi_boundaries
+from tsm.g2p import ToneSandhiG2P
+from tsm.util import alignment_to_tgt2src
+from tsm.util import cut_source_tokens_from_target_tokens_and_obtain_sandhi_boundaries
 from tsm.test_case import TSMTestCase
 import editdistance
 
@@ -20,27 +23,32 @@ class TestG2P(unittest.TestCase):
         total_distance = 0
         total_words = 0
         for pred, gold in zip(preds, golds):
+            pred = list(filter(lambda w: re.match(r'[a-zA-Z]+\d?', w), pred))
+            gold = list(filter(lambda w: re.match(r'[a-zA-Z]+\d?', w), gold))
             total_distance += editdistance.eval(pred, gold)
             total_words += len(gold)
         logger.info(f"{total_distance} {total_words}")
         return total_distance / total_words
 
-    def send_fixtures_to_g2p(self, test_case: Dict[str, Any]):
+    def send_fixtures_to_g2p(self, g2p: ToneSandhiG2P, test_case: Dict[str, Any]):
         tgt_tree_str = test_case["tgt_tree"]
         raw_src_text = test_case["src_text"]
-        src_tokens = Sentence.parse_mixed_text(raw_src_text, remove_punct=True)
+        src_tokens = Sentence.parse_mixed_text(raw_src_text)
         alignment = test_case["alignment"]
         tgt_to_src = alignment_to_tgt2src(alignment)
         written_pron = test_case["written_pronunciation"].split()
-        tgt_tree = ParentedTree.fromstring(tgt_tree_str)
-        src_word_lengths, src_sandhi_boundaries = get_src_sandhi_start_and_ends(tgt_tree, src_tokens, tgt_to_src)
-        pron = infer_pron(src_tokens, written_pron, src_word_lengths, src_sandhi_boundaries)
+        tgt_tree = Tree.fromstring(tgt_tree_str)
+        tgt_tree.pretty_print()
+        src_word_lengths, src_sandhi_boundaries = g2p.get_src_sandhi_start_and_ends(tgt_tree, src_tokens, tgt_to_src)
+        pron = g2p.infer_pron(src_tokens, written_pron, src_word_lengths, src_sandhi_boundaries)
         return pron
 
     def test_lexical_government(self):
-        tree = ParentedTree.fromstring("(S (NP (D the) (N dog)) (VP (V chased) (NP (D the) (N cat))))")
-        phrase_is_lexically_governed= lexical_government(tree)
-        boundaries = infer_sandhi_boundary(tree, phrase_is_lexically_governed)  
+        head_finder = ChineseHeadFinder()
+        g2p = ToneSandhiG2P(base_g2p=None, parser_url="", head_finder=head_finder)
+        tree = Tree.fromstring("(S (NP (D the) (N dog)) (VP (V chased) (NP (D the) (N cat))))")
+        phrase_is_lexically_governed= g2p.lexical_government(tree)
+        boundaries = g2p.infer_sandhi_boundary(tree, phrase_is_lexically_governed)  
         self.assertEqual(boundaries, [False, True, False, False, True])
 
     def test_get_character_level_sandhi_start_and_ends_from_tree(self):
@@ -48,18 +56,20 @@ class TestG2P(unittest.TestCase):
         with open(sandhi_test_json, "r") as f:
             test_cases = json.load(f)
 
-        start_and_ends_list = []
-        gold_start_and_ends_list = [test_case["sandhi_domain_start_ends"] for test_case in test_cases]
+        head_finder = ChineseHeadFinder()
+        g2p = ToneSandhiG2P(base_g2p=None, parser_url="", head_finder=head_finder)
+        #start_and_ends_list = []
+        #gold_start_and_ends_list = [test_case["sandhi_domain_start_ends"] for test_case in test_cases]
         actual_prons = [test_case["actual_pronunciation"].split() for test_case in test_cases]
         inferred_prons = []
         for test_case in test_cases:
-            pron = self.send_fixtures_to_g2p(test_case)
+            pron = self.send_fixtures_to_g2p(g2p, test_case)
             inferred_prons.append(pron)
         try:
             assert inferred_prons == actual_prons
         except AssertionError as e:
             print(e)
-        assert self.term_error_rate(inferred_prons, actual_prons) < 0.1
+        assert self.term_error_rate(inferred_prons, actual_prons) < 0.05
 
     def test_cut_source_tokens_from_target_tokens_and_obtain_sandhi_boundaries(self):
         src_tokens = Sentence.parse_mixed_text("到厝了後愛會記得用Line敲予我")
@@ -78,6 +88,8 @@ class TestG2P(unittest.TestCase):
         with open(suisiann_test_json, "r") as f:
             test_cases = json.load(f)
 
+        head_finder = ChineseHeadFinder()
+        g2p = ToneSandhiG2P(base_g2p=None, parser_url="", head_finder=head_finder)
         inferred_prons = []
         actual_prons = [test_case["actual_pronunciation"].split() for test_case in test_cases]
         for test_case in test_cases:
@@ -85,7 +97,7 @@ class TestG2P(unittest.TestCase):
             graph_text, phn_text = Sentence.get_grapheme_phoneme_pairs(sent_obj)
             test_case["src_text"] = graph_text
             test_case["written_pronunciation"] = phn_text
-            inferred_pron = self.send_fixtures_to_g2p(test_case)
+            inferred_pron = self.send_fixtures_to_g2p(g2p, test_case)
             inferred_prons.append(inferred_pron)
         try:
             assert inferred_prons == actual_prons
